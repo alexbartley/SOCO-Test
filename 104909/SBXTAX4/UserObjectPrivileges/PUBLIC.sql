85db505e02f66634824bf2a80e74dd0ec38fd795
alexbartley <alex.bartley@red-gate.com>
2018-05-15 19:14:15 +0100

    Update PUBLIC.sql

--- 104909/SBXTAX4/UserObjectPrivileges/PUBLIC.sql
+++ 104909/SBXTAX4/UserObjectPrivileges/PUBLIC.sql
@@ -1,4 +1,1 @@
-GRANT SELECT ON sbxtax4.sqln_explain_plan TO "PUBLIC";
-GRANT INSERT ON sbxtax4.sqln_explain_plan TO "PUBLIC";
-GRANT UPDATE ON sbxtax4.sqln_explain_plan TO "PUBLIC";
-GRANT DELETE ON sbxtax4.sqln_explain_plan TO "PUBLIC";
+
